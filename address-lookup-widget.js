46ae173fa607ecddb3cea366f2ae25aaa2b8680a
Katie Taylor <katiemxtaylor@gmail.com>
2021-07-26 15:59:13 -0700

    add basic ui

--- address-lookup-widget.js
+++ address-lookup-widget.js
@@ -3,23 +3,31 @@
 */
 
 // console.log('hello world');
-let url = 'https://geocoding.geo.census.gov/geocoder/locations/onelineaddress?address=4600+Silver+Hill+Rd%2C+Washington%2C+DC+20233&benchmark=2020&format=json';
-$.ajax({
-    url: url,
-    method: 'GET',
-    dataType: 'json',
-    crossDomain: true,
-    // beforeSend: function() {
-    // }
-    // ,
-    success: function( result ) {
-        console.log(result);
-    }
-    ,
-    error: function( err ){
-      console.log(err);
-    }
-    // ,
-    // complete: function() {
-    // }
+$('#addressSubmit').on('click', function() {
+    let streetAddress = document.getElementById("addressInput").value;
+    console.log(`streetAddress: ${streetAddress}`);
+    // let streetAddress = `4600 Silver Hill Rd, Washington, DC 20233`;
+    let benchmark = '2020';
+    let vintage = 'Census2020_Census2020';
+    let url = encodeURI(`https://geocoding.geo.census.gov/geocoder/locations/onelineaddress?address=${streetAddress}&benchmark=${benchmark}&format=json`);
+    // let url = 'https://geocoding.geo.census.gov/geocoder/locations/onelineaddress?address=4600+Silver+Hill+Road%2C+Washington+DC+20233&benchmark=2020&vintage=Census2020_Census2020&format=json';
+    $.ajax({
+        url: url,
+        method: 'GET',
+        dataType: 'json',
+        crossDomain: true,
+        // beforeSend: function() {
+        // }
+        // ,
+        success: function( result ) {
+            console.log(result);
+        }
+        ,
+        error: function( err ){
+        console.log(err);
+        }
+        // ,
+        // complete: function() {
+        // }
+    });
 });
